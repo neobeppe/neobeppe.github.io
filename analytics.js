c2b59c423068537f4257cf4f2ff7e7bcf19d4337
Giuseppe Travasoni <giuseppe.travasoni@gmail.com>
2024-12-03 12:01:02 +0100

    analytics in js and instagram redirect

---
+++ analytics.js
@@ -0,0 +1,13 @@
+(function() {
+    var gaScript = document.createElement('script');
+    gaScript.async = true;
+    gaScript.src = "https://www.googletagmanager.com/gtag/js?id=UA-5983182-3";
+    document.head.appendChild(gaScript);
+
+    gaScript.onload = function() {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-5983182-3');
+    };
+})();
