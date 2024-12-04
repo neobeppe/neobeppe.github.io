e389044ca62dfdb38569eddf7475ada07cefcfb8
Giuseppe Travasoni <giuseppe.travasoni@gmail.com>
2024-12-04 15:20:09 +0100

    edit contents

--- assets/languages.js
+++ assets/languages.js
@@ -3,9 +3,8 @@ const languages = {
         body: `
             <br/>
             <p>Hello, my name is Giuseppe Travasoni but you can call me Beppe</p>
-            <p>I've co-founded and I'm running <a href="https://truescreen.app" target="_blank">TrueScreen</a> and <a href="https://beatcode.it" target="_blank">Beatcode</a></p>
-            <p>Previously, I've worked as Senior iOS Developer and Team Leader at <a href="http://ynap.com" target="_blank">YOOX NET-A-PORTER Group</a>, <a href="http://shopfullygroup.com" target="_blank">ShopFully</a>, and some other companies</p>
-            <p>You can get in touch with me on <a href="https://www.linkedin.com/in/giuseppetravasoni/" target="_blank">LinkedIn</a> or via email at <a id="mailto"></a></p>
+            <p>I've co-founded and I'm running two startups: <a href="https://truescreen.app" target="_blank">TrueScreen</a> and <a href="https://beatcode.it" target="_blank">Beatcode</a>
+            <br/>You can get in touch with me on <a href="https://www.linkedin.com/in/giuseppetravasoni/" target="_blank">LinkedIn</a> or via email at <a id="mailto"></a></p>
             <p>In my free time, I ride my road bike for <a href="https://teamkannelloni.com/" target="_blank">Team Kannelloni</a>, raising funds for <a href="https://www.ail.it/" target="_blank">AIL</a>, and post some photos on my <a href="https://www.instagram.com/beppe_travasoni/" target="_blank">Instagram account</a></p>
             <p>Best,<br/>Beppe<span class="blinking-cursor">|</span></p>
         `
@@ -14,9 +13,8 @@ const languages = {
         body: `
             <br/>
             <p>Ciao, mi chiamo Giuseppe Travasoni ma puoi chiamarmi Beppe</p>
-            <p>Ho co-fondato e gestisco <a href="https://truescreen.app" target="_blank">TrueScreen</a> e <a href="https://beatcode.it" target="_blank">Beatcode</a></p>
-            <p>In passato, ho lavorato come Senior iOS Developer e Team Leader presso <a href="http://ynap.com" target="_blank">YOOX NET-A-PORTER Group</a>, <a href="http://shopfullygroup.com" target="_blank">ShopFully</a> e altre aziende</p>
-            <p>Puoi contattarmi su <a href="https://www.linkedin.com/in/giuseppetravasoni/" target="_blank">LinkedIn</a> o via email a <a id="mailto"></a></p>
+            <p>Ho co-fondato e gestisco due startup: <a href="https://truescreen.app" target="_blank">TrueScreen</a> e <a href="https://beatcode.it" target="_blank">Beatcode</a>
+            <br/>Puoi contattarmi su <a href="https://www.linkedin.com/in/giuseppetravasoni/" target="_blank">LinkedIn</a> o via email a <a id="mailto"></a></p>
             <p>Nel mio tempo libero, corro in bici da corsa per il <a href="https://teamkannelloni.com/" target="_blank">Team Kannelloni</a>, raccogliendo fondi per <a href="https://www.ail.it/" target="_blank">AIL</a>, e pubblico alcune foto sul mio <a href="https://www.instagram.com/beppe_travasoni/" target="_blank">profilo Instagram</a></p>
             <p>A presto,<br/>Beppe<span class="blinking-cursor">|</span></p>
         `
